77e5d9434f8ca9f1ada8b334a57f819fc782b2b8
VerrroS <verrros@gmail.com>
2021-12-16 11:31:45 +0100

    add pylint check

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ from pybuilder.core import use_plugin, init
 use_plugin("python.core")
 use_plugin("python.unittest")
 #use_plugin("python.coverage")
+use_plugin("python.pylint")
 
 
 name = "catcher"

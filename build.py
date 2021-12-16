fd95144b98d5b20c48a871840496a4901a6a2617
VerrroS <verrros@gmail.com>
2021-12-16 10:54:49 +0100

    update setup to support pybuilder

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
 use_plugin("python.unittest")
-use_plugin("python.coverage")
+#use_plugin("python.coverage")
 
 
 name = "catcher"
@@ -12,7 +12,7 @@ default_task = "publish"
 
 @init
 def set_properties(project):
-    project.set_property("dir_source_main_python", ".")
-    project.set_property("dir_source_unittest_python", ".")
+    project.set_property("dir_source_main_python", "main")
+    project.set_property("dir_source_unittest_python", "test")
     project.set_property("dir_source_main_scripts", ".")
     project.set_property("dir_docs", ".")
